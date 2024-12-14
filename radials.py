5e587f1a70eb4d887fb4b81b95c48f99be9057d7
Lorenzo Corgnati <lorenzo.corgnati@sp.ismar.cnr.it>
2024-12-14 11:25:58 +0100

    Alignment of new machine

--- radials.py
+++ radials.py
@@ -863,7 +863,7 @@ class Radial(fileParser):
 
         
         # Add title
-        plt.title(self.file_name + ' total velocity field', fontdict={'fontsize': 30, 'fontweight' : 'bold'}, pad=25)
+        plt.title(self.file_name + ' radial velocity field', fontdict={'fontsize': 30, 'fontweight' : 'bold'}, pad=25)
 
                 
         if show:
